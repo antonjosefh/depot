c096933a7301135e669a210e9f1f898091a3be33
Anton J Hofstetter <antonjosefh@gmail.com>
2014-07-14 10:14:56 -0700

    add product images

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,23 @@ Product.delete_all
 Product.create!(
   title: "Programming Ruby 1.9",
   description: %{<p>Ruby is the fastest growing and most exciting dynamic language out there. If you need to get working programs delivered fast, you should add Ruby to your toolbox.</p>},
-  image_url: 'ruby.png',
+  image_url: 'ruby.jpg',
   price: 49.95)
+
+Product.create!(
+  title: "Agile Rails Development 4",
+  description: %{<p>Rails is awesome and you should read this book if you want to be awesome, too."</p>},
+  image_url: 'agile.jpg',
+  price: 58.95)
+
+Product.create!(
+  title: "The Pragmatic Programmer",
+  description: %{<p>This book is the shit. Classic.</p>},
+  image_url: 'pragmatic.jpg',
+  price: 29.95)
+
+Product.create!(
+  title: "Revenge of the Babysat",
+  description: %{<p>If you don't know what this is, you make me sad.</p>},
+  image_url: 'babysat.jpg',
+  price: 17.99)
